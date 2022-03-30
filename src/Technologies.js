b9455a8665a339b9c5bc98f5d0dbd0bc8cc11a35
Polina Guseva <polina_0901@mail.ru>
2022-03-30 20:27:34 +0300

    Components

---
+++ src/Technologies.js
@@ -0,0 +1,13 @@
+const Technologies = () => {
+  return (
+    <div>
+    <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+    </ul>
+    </div>
+  )
+}
+
+export default Technologies
