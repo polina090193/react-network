40c9712857ee708dd22a75e9b37495d913464ebf
Polina Guseva <polina_0901@mail.ru>
2022-04-04 14:17:31 +0300

    Improve main menu

--- src/components/Sidebar/MainMenuButton/MainMenuButton.jsx
+++ src/components/Sidebar/MainMenuButton/MainMenuButton.jsx
@@ -2,10 +2,14 @@ import * as React from 'react';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
 import { Link } from "react-router-dom";
+import MainMenuButtonCss from './MainMenuButton.module.css'
+import { useLocation } from "react-router-dom"
 
 const MainMenuButton = (props) => {
+  const location = useLocation()
+
   return (
-    <ListItemButton component={Link} to={props.link}>
+    <ListItemButton component={Link} to={props.link} selected={props.link === location.pathname}>
       <ListItemText primary={props.title} />
     </ListItemButton>
   )
