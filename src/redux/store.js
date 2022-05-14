38a7e09ece788d3f86082a1f5371c17acab56554
Polina Guseva <polina_0901@mail.ru>
2022-05-14 20:15:17 +0300

    Thunks

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import profileReducer from './reducers/profile-reducer'
 import messagesReducer from './reducers/messages-reducer'
 import usersReducer from "./reducers/users-reducer";
+import thunk from "redux-thunk";
 
 const reducers = combineReducers({
   profilePage: profileReducer,
@@ -9,6 +10,6 @@ const reducers = combineReducers({
   usersPage: usersReducer,
 })
 
-const store = createStore(reducers, applyMiddleware())
+const store = createStore(reducers, applyMiddleware(thunk))
 
 export default store
